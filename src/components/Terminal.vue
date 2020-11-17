cd10acfd87e987342efa0619a545189f10a6a933
juanan <juanan@mentalrepublic.com>
2020-11-17 14:03:19 +0100

    🌚 Testing litte changes

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -26,11 +26,17 @@ export default {
     async created() {
         this.terminal.lines = []
         this.changeStatePrompt()
-        await this.typetext({param01:'Starting server',speed:30,color:'info'})
+        await this.typetext({param01:"Starting proccess",speed:30,color:'info'})
+        await this.sleep(120)
+        await this.typetext({param01:"Meditating",speed:35,color:'success'})
         await this.sleep(120)
         await this.loadingEffect({text:'...',speed:200,repeats:2})
         await this.sleep(40)
-        this.changeStatePrompt()
+        await this.typetext({param01:"Starting servers",speed:35,color:'info'})
+        await this.sleep(120)
+        await this.loadingEffect({text:'...',speed:200,repeats:2})
+        await this.sleep(40)
+        this.changeStatePrompt()  
     },
     computed: {
         ...mapState(['terminal','sleep'])
@@ -43,92 +49,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import './../assets/fonts/fonts.scss';
-@import './../assets/scss/variables.scss';
-@import './../assets/scss/keyframes.scss';
-
-.terminal{
-    display: block;
-    color:$color_text;
-    width: 100%;
-    font-family: IBMVBA8 , monospace;
-    font-size: 25px;
-    min-height: 100%;
-    .prompt {
-        width: auto;
-        margin-right: .4em;
-        float: left;
-    }
-    .typed {
-        padding: .8em;
-    }
-    .overlay {
-        background-color:$color_bg;
-        position:fixed;
-        width:100%;
-        height:100%;
-        top:0px;
-        left:0px;
-        z-index:-10;
-        transition: $effect_background_color;
-        -o-transition: $effect_background_color;
-        -moz-transition: $effect_background_color;
-        -webkit-transition: $effect_background_color;
-    }
-    &:focus-within > .overlay{
-        background-color: $color_bg_light;
-    }
-    .line{
-        width: 100%;
-        .texted{
-            word-break: break-all;
-        }
-        .fakeinput {
-            word-break: break-all;
-            float: left;
-            &:after {
-                content: "";
-                display: inline-block;
-                width: .5em;
-                background-color: #cccccc;
-                height: 1.4rem;
-                vertical-align: text-top;
-                margin-left: .2em;
-            }
-        }
-    }
-    &:focus-within .fakeinput:after{
-        animation: blinkerMadaFaka 1.2s linear infinite;
-        -webkit-animation: blinkerMadaFaka 1.2s linear infinite;
-        width: .2em!important;
-    }
-    div,span {
-        &.info{
-            color:$color_info!important
-        }
-        &.success{
-            color:$color_success!important
-        }
-        &.error{
-            color:$color_error!important
-        }
-        &.error-light{
-            color:$color_error_light!important
-        }
-    }
-    input{
-        width: auto;
-        background: transparent;
-        color: transparent;
-        border: 0;
-        padding: 0;
-        display: block;
-        float: left;
-        font-family: IBMVBA8 , monospace;
-        font-size: 25px;
-        &:focus{
-            outline: none;
-        }
-    }
-}
+@import './../assets/terminal.scss';
 </style>

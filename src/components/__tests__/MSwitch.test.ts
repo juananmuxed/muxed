6aca2de9de4b443f95b209926f1be12924fd7d5a
MuXeD <muxed@muxed.dev>
2024-03-30 02:42:07 +0100

    🎨 Format tests

--- src/components/__tests__/MSwitch.test.ts
+++ src/components/__tests__/MSwitch.test.ts
@@ -1,8 +1,8 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect } from 'vitest';
 
-describe("Switch", () => {
-  describe("Props", () => {
-    it("Run mswitch", () => {
+describe('Switch', () => {
+  describe('Props', () => {
+    it('Run mswitch', () => {
       expect(1 + 2).not.toBe(2);
     });
   });
